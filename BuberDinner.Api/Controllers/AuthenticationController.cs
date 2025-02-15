0f629355de95992669524ae5990961d1a9682a67
Isaias Pinto <isaiascxs10@gmail.com>
2025-02-15 13:41:51 -0300

    auth

--- BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -1,47 +1,21 @@
-﻿using BuberDinner.Application.Authentication.Commands.Register;
-using BuberDinner.Application.Authentication.Queries.Login;
-using BuberDinner.Contracts.Authentication;
-using MapsterMapper;
-using MediatR;
-using Microsoft.AspNetCore.Authorization;
+﻿using BuberDinner.Contracts.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
+[ApiController]
 [Route("auth")]
-[AllowAnonymous]
-public class AuthenticationController : ApiController
+public class AuthenticationController : ControllerBase
 {
-    private readonly ISender _mediator;
-    private readonly IMapper _mapper;
-
-    public AuthenticationController(ISender mediator, IMapper mapper)
-    {
-        _mediator = mediator;
-        _mapper = mapper;
-    }
-
     [HttpPost("register")]
-    public async Task<IActionResult> Register(RegisterRequest request)
+    public IActionResult Register(RegisterRequest request)
     {
-        var command = _mapper.Map<RegisterCommand>(request);
-
-        var registerResult = await _mediator.Send(command);
-
-        return registerResult.Match(
-            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
-            errors => Problem(errors));
+        return Ok();
     }
-
+     
     [HttpPost("login")]
-    public async Task<IActionResult> Login(LoginRequest request)
+    public IActionResult Login(LoginRequest request)
     {
-        var query = _mapper.Map<LoginQuery>(request);
-
-        var authResult = await _mediator.Send(query);
-
-        return authResult.Match(
-            authResult => Ok(_mapper.Map<AuthenticationResponse>(authResult)),
-            errors => Problem(errors));
+        return Ok();
     }
 }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,20 +1,10 @@
-using BuberDinner.Api;
-using BuberDinner.Application;
-using BuberDinner.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
-    .AddPresentation()
-    .AddAplication()
-    .AddInfrastructure(builder.Configuration);
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.MapControllers();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.Run();
